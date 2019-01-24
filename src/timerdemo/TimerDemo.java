76d381c99dbadb0e94f90899261ccca2ab8f7539
Hydra <Hydra@TORUS>
2019-01-24 21:12:39 +0700

    Timer demo

---
+++ src/timerdemo/TimerDemo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package timerdemo;
+
+/**
+ *
+ * @author Hydra
+ */
+public class TimerDemo {
+
+    static MainFrame frMain;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frMain=new MainFrame();
+        frMain.setVisible(true);
+    }
+    
+}
